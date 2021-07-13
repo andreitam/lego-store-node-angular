bc9375b39d330478ad5b97cef3ab5023a1774926
andrei_tamasanu@yahoo.com <Anamaria23646>
2021-07-13 21:41:40 +0300

    Added starting db

---
+++ onlinestore.sql
@@ -0,0 +1,83 @@
+CREATE DATABASE `onlinestore` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+-- onlinestore.theme definition
+
+CREATE TABLE `theme` (
+  `theme_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(16) NOT NULL,
+  `description` varchar(1024) DEFAULT NULL,
+  `picture_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`theme_id`),
+  KEY `theme_FK` (`picture_id`),
+  CONSTRAINT `theme_FK` FOREIGN KEY (`picture_id`) REFERENCES `picture` (`picture_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- onlinestore.product definition
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `price` float NOT NULL,
+  `discount` int(11) DEFAULT NULL,
+  `rating` float DEFAULT NULL,
+  `age` int(11) NOT NULL,
+  `piece_count` int(11) NOT NULL,
+  `availability` varchar(32) NOT NULL,
+  `description` varchar(1024) DEFAULT NULL,
+  `picture_id` int(11) DEFAULT NULL,
+  `theme_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`product_id`),
+  KEY `product_FK` (`picture_id`),
+  KEY `product_FK_1` (`theme_id`),
+  CONSTRAINT `product_FK` FOREIGN KEY (`picture_id`) REFERENCES `picture` (`picture_id`),
+  CONSTRAINT `product_FK_1` FOREIGN KEY (`theme_id`) REFERENCES `theme` (`theme_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- onlinestore.picture definition
+
+CREATE TABLE `picture` (
+  `picture_id` int(11) NOT NULL AUTO_INCREMENT,
+  `picture_url1` varchar(128) DEFAULT NULL,
+  `picture_url2` varchar(128) DEFAULT NULL,
+  `picture_url3` varchar(128) DEFAULT NULL,
+  `picture_url4` varchar(128) DEFAULT NULL,
+  `picture_url5` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`picture_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- onlinestore.customer definition
+
+CREATE TABLE `customer` (
+  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `address` varchar(128) NOT NULL,
+  `city` varchar(32) NOT NULL,
+  `postal_code` varchar(32) NOT NULL,
+  `phone` varchar(32) DEFAULT NULL,
+  `rights` tinyint(1) NOT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- onlinestore.`order` definition
+
+CREATE TABLE `order` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `total` double DEFAULT NULL,
+  `customer_id` int(11) NOT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `order_FK` (`customer_id`),
+  CONSTRAINT `order_FK` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- onlinestore.order_product definition
+
+CREATE TABLE `order_product` (
+  `order_product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `price` float DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `subtotal` double DEFAULT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  PRIMARY KEY (`order_product_id`),
+  KEY `order_product_FK` (`order_id`),
+  KEY `order_product_FK_1` (`product_id`),
+  CONSTRAINT `order_product_FK` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`),
+  CONSTRAINT `order_product_FK_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
